76832b08e852aa047c618f92d23a3e9274f36f83
Abid Hasan <abidh8820@gmail.com>
2024-01-23 15:20:22 +0600

    added text to mardown functionality

--- textExtractor.py
+++ textExtractor.py
@@ -1,9 +1,21 @@
 from autoocr import AutoOCR
 
-oa = AutoOCR(lang='Bengali')
-oa.set_datapath('/usr/local/Cellar/tesseract/4.0.0_1/share/tessdata')
-oa.set_datapath('/path/to/tessdata')
+oa = AutoOCR(lang='bangla') 
+oa.set_datapath('/usr/share/tesseract-ocr/5/tessdata')
 
 def getText(image_path):
     text = oa.get_text(image_path)
     return text
+
+def append_to_markdown(file_path, text):
+    try:
+        with open(file_path, 'a', encoding='utf-8') as markdown_file:
+            markdown_file.write(text + '\n\n')
+    except Exception as e:
+        print(f'Error appending text to {file_path}: {e}')
+
+
+markdown_file_path = 'output.md'
+text_to_append = (getText('sample.png'))
+
+append_to_markdown(markdown_file_path, text_to_append)

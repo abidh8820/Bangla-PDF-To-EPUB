ba1cd7e84d7f3aa6fc0f9f191f830c482b65c9dc
ssshanto <ssshanto@gmail.com>
2024-01-24 20:04:05 +0600

    ImageExtractor automatically removes nonparagraph text

--- ImageExtractor.py
+++ ImageExtractor.py
@@ -1,6 +1,6 @@
-
 import os
 from pdf2image import convert_from_path
+from LayoutAnalysis import *
 
 def convert_pdf_to_images(input_file, output_directory, dpi=300):
     # Check if the output directory exists, and create it if not
@@ -9,9 +9,13 @@ def convert_pdf_to_images(input_file, output_directory, dpi=300):
 
     # Convert PDF to images
     pages = convert_from_path(input_file, dpi)
+    # TODO: Optimize this to use numpy arrays directly instead of PIL's image libraries
 
     # Save images in the specified directory as PNG
     for count, page in enumerate(pages):
+        page = np.array(page)
+        page = extractParagraphsFromImage(page)
+        page = Image.fromarray(page)
         image_path = os.path.join(output_directory, f'page{count}.png')
         page.save(image_path, 'PNG')
 
--- LayoutAnalysis.py
+++ LayoutAnalysis.py
@@ -7,7 +7,30 @@ from PIL import Image
 weight_path = "best.pt"
 model = YOLO(weight_path)
 
-def extractParagraphs(image_path):
+def extractParagraphsFromImage(image: np.ndarray):
+    # image = np.array(image)
+    paragraph_image = np.ones(image.shape)
+    # fig, ax = plt.subplots(1, 2, figsize=(10, 5))
+
+    # ax[0].imshow(image)
+    # ax[0].set_title("OG Image")
+
+    with torch.no_grad():
+        predictions = model(image)
+
+    for r in predictions:
+        coords, labels = r.boxes.xyxy, r.boxes.cls
+        num = len(coords)
+        for i in range(num):
+            x_min, y_min, x_max, y_max = coords[i]
+            x_min, y_min, x_max, y_max = int(x_min.item()), int(y_min.item()), int(x_max.item()), int(y_max.item())
+            if labels[i] == 0:
+                paragraph_image[y_min:y_max, x_min:x_max] = image[y_min:y_max, x_min:x_max] / 255.0
+
+    paragraph_image = (paragraph_image * 255).astype(np.uint8)
+    return paragraph_image
+
+def extractParagraphsFromPath(image_path: str):
     image = np.array(Image.open(image_path).convert('RGB'))
     paragraph_image = np.ones(image.shape)
     # fig, ax = plt.subplots(1, 2, figsize=(10, 5))
@@ -29,6 +52,6 @@ def extractParagraphs(image_path):
 
     return paragraph_image
 
-clean_image = extractParagraphs('sample.png')
-plt.imshow(clean_image)
-plt.show()
+# clean_image = extractParagraphsFromImage('sample.png')
+# plt.imshow(clean_image)
+# plt.show()
